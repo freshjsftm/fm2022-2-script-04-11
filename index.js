a20a8d4d29ff88c706572c898f3f121a65452b9e
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-09 12:29:56 +0200

    Add jsdoc

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 let userInput1 = prompt("Enter first number: ", 2);
 let userInput2 = prompt("Enter second number: ", 6);
-
+//debugger
 if (checkUserInputNumber(userInput1) && checkUserInputNumber(userInput2)) {
   const summa = getSumTwoNumbers(Number(userInput1), Number(userInput2));
   console.log(summa);
 } else {
   console.log("error");
 }
-
+/**
+ * 
+ * @param {number} val1 
+ * @param {number} val2 
+ * @returns {number}
+ */
 function getSumTwoNumbers(val1, val2) {
   return val1 + val2;
 }
-
+/**
+ * 
+ * @param {string} value 
+ * @returns {boolean}
+ */
 function checkUserInputNumber(value) {
   if (value === "" || isNaN(value) || value === null) {
     return false;
