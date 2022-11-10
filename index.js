fe3ed9122a09dbee3c5289551683056602487dd6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-10 10:17:30 +0200

    Add qetPerimetrSquare

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 //написати функцію, яка обчислює периметр квадрату
-
-const userInput = prompt("enter side square:"); //string | null
-console.log(userInput);
-
-//перетворити те, що вводить користувач на числовий тип number
-//console.log(Number(userInputSide))//NaN
-//функція повинна повертати лише тип number
+const userInput = '2'; //prompt("enter side square:"); //string | null
 const leadToNumber = function (value) {
   if (isNaN(Number(value)) || value === null || value === "" || value === " ") {
     return NaN;
   }
   return Number(value);
 };
-
-console.log(leadToNumber(userInput));
-console.log(leadToNumber(777));
+/**
+ * calculate and return perimetr
+ * @param {number} side default value = 1
+ * @returns {number | null}
+ */
+const qetPerimetrSquare = function (side=1) {
+  if(isNaN(side) || side<=0){
+    return null;
+  }
+  return side + side + side + side;
+};
+const userInputNumber = leadToNumber(userInput);
+console.log('perimetr', qetPerimetrSquare(userInputNumber));
+console.log('perimetr', qetPerimetrSquare(78));
+console.log('perimetr', qetPerimetrSquare());
+//console.log(qetPerimetrSquare(leadToNumber(userInput)));
