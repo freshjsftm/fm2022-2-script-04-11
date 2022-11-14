67659a5334a0e30de5abd01e595ea9bbaed7de4c
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-14 12:07:05 +0200

    Add example while/for

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
-function calculate(num1 = 10, num2 = 2, operator = "+") {
-  if (isNaN(num1 - num2)) {
-    return null;
-  }
-  let f = null;
-  switch (operator) {
-    case "+":
-      f = summa;
-      break;
-    case "-":
-      f = substr;
-      break;
-    case "/":
-      f = div;
-      break;
-    case "*":
-      f = mult;
-      break;
-    case "%":
-      f = rem;
-      break;
-    default:
-      f = null;
-      break;
-  }
-  if(typeof f === 'function'){
-    return f(num1, num2) 
-  }
-  return null;
-}
-const rem = function (num1, num2) {
-  return num1 % num2;
-};
-const summa = function (num1, num2) {
-  return num1 + num2;
-};
-const substr = function (num1, num2) {
-  return num1 - num2;
-};
-const mult = function (num1, num2) {
-  return num1 * num2;
-};
-const div = function (num1, num2) {
-  return num1 / num2;
-};
+// let count = 0;
+// //debugger
+// while (true) {
+//   if(count % 2 === 1){
+//     count++;
+//     continue;
+//   }
+//   console.log(count);
+//   count++;
+//   if (count >= 5) {
+//     break;
+//   }
+// }
+// console.log("end");
 
-const result = calculate(15, undefined, "-");
-console.log(result);
-
-const getPerimetrRectangle = function(side1, side2){
-  return summa(side1,side2)*2;
-}
+// do {
+//   if(count % 2 === 1){
+//     count++;
+//     continue;
+//   }
+//   console.log(count);
+//   count++;
+//   // if (count >= 5) {
+//   //   break;
+//   // }
+// } while (count<5);
 
 //debugger
-const hightOrderFunction = function(val1, val2, func){
-  return func(val1, val2)
-}
+// for (let i = 0; i < 5; i++) {
+//   console.log(i)
+// }
 
-console.log(hightOrderFunction(7,5,mult))
+// while (true) {
+//   const result = prompt("enter 456");
+//   if (result === "456") {
+//     break;
+//   }
+// }
+
+//const PIN_CODE = "1111";
+// let count = 0;
+// while (true) {
+//   const result = prompt("enter pin");
+//   count++;
+//   if (count > 3) {
+//     console.log("error");
+//     break;
+//   }
+//   if (result === PIN_CODE) {
+//     console.log("welcome");
+//     break;
+//   }
+// }
+// console.log(count);
+
+// for (let i = 0; i < 4; i++) {
+//   const result = prompt("enter pin");
+//   if (result === PIN_CODE) {
+//     console.log("welcome");
+//     break;
+//   }
+//   if (i === 3) {
+//     console.log("error");
+//   }
+// }
