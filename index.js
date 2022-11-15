8e839479cc55d7c87c047beb570037c82b1981ba
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-15 10:33:13 +0200

    Add loop practice

--- index.js
+++ index.js
@@ -1,65 +1,48 @@
-// let count = 0;
-// //debugger
-// while (true) {
-//   if(count % 2 === 1){
-//     count++;
-//     continue;
-//   }
-//   console.log(count);
-//   count++;
-//   if (count >= 5) {
-//     break;
-//   }
-// }
-// console.log("end");
+//1950-2030
+// Високосный год кратен 4, но при этом не кратен 100, либо кратен 400. Иными словами, если год делится на 4 без остатка, но делится на 100 только с остатком, то он високосный, иначе — невисокосный, кроме случая, если он делится без остатка на 400 — тогда он всё равно високосный.
 
-// do {
-//   if(count % 2 === 1){
-//     count++;
-//     continue;
-//   }
-//   console.log(count);
-//   count++;
-//   // if (count >= 5) {
-//   //   break;
-//   // }
-// } while (count<5);
+const START_DIAPAZON = 2190;
+const END_DIAPAZON = 2210;
 
-//debugger
-// for (let i = 0; i < 5; i++) {
-//   console.log(i)
-// }
+for (let year = START_DIAPAZON; year <= END_DIAPAZON; year++) {
+  if(year%4===0 && year%100 !==0 || year%400===0){
+    console.log(year)
+  }
+}
+в зворотньому порядку вивести
+for (let year = END_DIAPAZON; year >= START_DIAPAZON; year--) {
+  if(year%4===0 && year%100 !==0 || year%400===0){
+    console.log(year)
+  }
+}
 
-// while (true) {
-//   const result = prompt("enter 456");
-//   if (result === "456") {
-//     break;
-//   }
-// }
+//написати функцію, яка запитує в користувача число до тих пір, доки він його не введе
+//функція брудна, нічого не приймає, а повертає виключно коректне число числового типу!!!!!!
+// continue
+//в циклі
+//1 - запитую в користувача
+//2 - перевіряємо:
+//  а)  якщо введене є числом - виходимо і повертаємо число
+//  б)  якщо введене є помилкою - переходимо до наступної     ітерації, в іншому випадку повертаємо число
 
-//const PIN_CODE = "1111";
-// let count = 0;
-// while (true) {
-//   const result = prompt("enter pin");
-//   count++;
-//   if (count > 3) {
-//     console.log("error");
-//     break;
-//   }
-//   if (result === PIN_CODE) {
-//     console.log("welcome");
-//     break;
-//   }
-// }
-// console.log(count);
+const enterYourNumber = function () {
+  while (true) {
+    const num = prompt("enter your number");
+    if (isNaN(Number(num)) || num === null || num === "" || num === " ") {
+      continue;
+    }
+    return Number(num);
+  }
+};
+console.log(enterYourNumber());
 
-// for (let i = 0; i < 4; i++) {
-//   const result = prompt("enter pin");
-//   if (result === PIN_CODE) {
-//     console.log("welcome");
-//     break;
-//   }
-//   if (i === 3) {
-//     console.log("error");
-//   }
-// }
+
+const enterYourNumber2 = function () {
+  while (true) {
+    const num = prompt("enter your number");
+    if (isNaN(Number(num))===false && num !== null && num !== "" && num !== " ") {
+      return Number(num);
+    } 
+  }
+};
+console.log(enterYourNumber2());
