d7fc90d735e358b02f60f588aa8e69c3b5e03680
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-18 10:16:56 +0200

    Add object prototype

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-// const colors = {
-//   0: "red",
-//   1: "lime",
-//   2: "blue",
-//   3: "teal",
-// };
+const myArrayPrototype = {
+  push : function(value){
+    this[this.length] = value;
+    this.length++;
+    return this.length;
+  }
+}
 
-// //console.log(colors[2])
+function MyArray() {
+  this.length = 0; 
+}
 
-// //add new colors
-// colors[4] = 'white'
-// colors[5] = 'pink'
+MyArray.prototype = myArrayPrototype;
 
-// for (const key in colors) {
-//   console.log(colors[key])
-// }
+const array = [2]; //instance
+const array2 = [2];
+console.log(typeof array.push)
+//console.dir(array.push)
+console.log(array.push === array2.push);
 
-const array1 = ["red","lime","blue","teal"];
-const array2 = new Array("red","lime","blue","teal");
-const array3 = new Array(5);
+const myArray = new MyArray();
+const myArray2 = new MyArray();
+myArray.push(404);
+myArray.push(456);
+// console.log(myArray);
+console.log(myArray.push === myArray2.push);
