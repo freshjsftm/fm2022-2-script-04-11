0452ce066b265005dd9c1ec97ccd2113f24e4d95
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-21 11:50:32 +0200

    Add join & checkKeysInRow

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const languages = [ 'en', 'ua', 'fr', 'pl','ua' ];
-//includes
-const checkLang = languages.includes('pl');// true false
-if(checkLang){
-  console.log('dzen\' dobruy')
-}else{
-  console.log('error')
+const languages = ["en", "ua", "fr", "fr", "pl", "ua"];
+
+//join
+const string = languages.join('');
+
+//написати функцію, яка приймає масив, і повертає true, якщо в масиві є два однакових значення, які ідуть поспіль, інакше - повертає false
+
+function checkKeysInRow(array) {
+  //для кожного елементу треба зробити наступну перевірку
+  for (let index = 0; index < array.length; index++) {
+    // поточний === наступний
+    if (array[index] === array[index + 1]) {
+      //якщо правда то завершити роботу і повернути true
+      return true;
+    }
+  }
+  //якщо дійшли до кінця масиву і не знайшли поспіль однакових, то повертаємо false  
+  return false;
 }
-
-//reverse
-const reverseLanguages = languages.reverse(); //address
-languages.unshift('fr');
-
-//indexOf
-const findIndex1 = languages.indexOf('ua');
-console.log(findIndex1)
-//lastIndexOf
-const findIndex2 = languages.lastIndexOf('ua');
-console.log(findIndex2)
-
-//написати функцію checkKeyAtArray, яка отримує в якості аргументів массив і ключ(елемент, який треба знайти) і повертає true,якщо ключ є в масиві, і false, якщо ключа не має в масиві
-//checkKeyAtArray(languages, 'en') => true
-
-const checkKey = function(arr,key){
- return arr.includes(key);
-}
-
-
