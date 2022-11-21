f273d44889881834b33affa6e86c872c532429f3
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-21 10:49:24 +0200

    Add pop in MyArrayPrototype

--- MyArray.js
+++ MyArray.js
@@ -3,16 +3,28 @@ function MyArrayPrototype() {
     this[this.length++] = value;
     return this.length;
   };
+  this.pop = function(){
+    //перевіряємо довжину, щоб вона не стала від'ємною
+    if(this.length === 0){
+      return; //return undefined; - так не пишуть!!!
+    }
+    //отримую останній і зберігаю у змінну
+    const lastItem = this[this.length-1];
+    //видаляю останній
+    delete this[this.length-1];
+    //змінюю довжину
+    this.length--; //this.length = this.length-1;
+    //повертаю останній зі змінної
+    return lastItem;
+  }
 }
-
 function MyArray() {
   this.length = 0;
 }
-
 MyArray.prototype = new MyArrayPrototype();
-
-
 const myArr = new MyArray();
 myArr.push(12);
+myArr.push(56);
+
 
 
