4cb120dbc1f026c666d8719d910f888ac2208051
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-21 10:01:14 +0200

    Add concat, fill, push, pop, shift, unshift

--- index.js
+++ index.js
@@ -1,64 +1,26 @@
-//об'єкт з логікою
-function FuncForUser() {
-  this.fullName = function () {
-    return `${this.fname} ${this.lname}`;
-  };
-}
+const arrNums1 = [1, 2, 3];
+const arrNums2 = [9, 8, 7];
+const arrNums3 = [4, 5, 6];
+//concat
+const resultArrConcat = arrNums1.concat(arrNums2, 12, arrNums3);
 
-//об'єкт з данними
-function User(fname, lname) {
-  this.fname = fname;
-  this.lname = lname;
-}
+//fill
+const newArray = new Array(5)
+                    .fill(0);
+//newArray.fill(0);
 
-//пов'язуємо
-User.prototype = new FuncForUser();
-
-const user = new User("Brad", "Pitt");
-const user2 = new User("Tom", "Rot");
-
-//створитий об'єкт чайник
-// властивості: колір, об'єм, мощність
-// методи: додати води, відлити води, працює
-
-function MethodForKattle() {
-  this.work = function () {
-    if (this.curentWater > 0) {
-      return "work with water " + this.curentWater;
-    }
-    return "alert! add water";
-  };
-  this.addWater = function (value) {
-    if (this.curentWater + value <= this.volume) {
-      this.curentWater += value;
-    }
-    return this.curentWater;
-  };
-  this.subWater = function (value) {
-    if (this.curentWater >= value) {
-      this.curentWater -= value;
-    }
-    return this.curentWater;
-  };
-  this.subWaterStas = function (value) {
-    if (this.currentWater - value >= 0) {
-      this.currentWater -= value;
-    }
-    return this.currentWater;
-  };
-}
-function Kattle(color, volume, power) {
-  this.color = color;
-  this.volume = volume;
-  this.power = power;
-  this.curentWater = 0;
-}
-Kattle.prototype = new MethodForKattle();
-
-const kattle = new Kattle("pink", 0.5, 800);
-console.log(kattle.work());
-kattle.addWater(123);
-console.log(kattle.work());
+//pop
+const deletedElementLast = arrNums1.pop();
+//shift
+const deletedElementFirst = arrNums2.shift();
+//unshift
+arrNums1.unshift(15,45,78);
 
 
 
+const languages = ['ua', 'fr', 'pl', 'en'];
+//мову з першого місця переставити на останнє
+//витягнути перше значення масива
+//це значення помістити в кінець
+const firstLang = languages.shift();
+languages.push(firstLang); //languages.push('ua');
