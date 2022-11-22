339bfeccb26254f4b7fe94d87d3170f71466617d
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-22 11:43:57 +0200

    Add resolve task

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-function sum(){
-  console.log(arguments)// властивість функції
-  let resultSumma = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    resultSumma += arguments[i]
+const nums = [1, 2, 5, 8, 5, 9, 6, 3, 2, 5];
+
+//slice
+const newArr = nums.slice(1, 2);
+console.log(newArr);
+console.log(nums);
+
+//splice
+//nums.splice(0) //0 - починаючи з цього індексу видали усе до кінця
+//nums.splice(0, 2) //2 - кількість видаляємих елементів
+nums.splice(3, 0, 111, 112, 113, 114); // 111, 112, 113, 114 - ці елементи будуть додані, почина.чи з вказаного індекса(3), видалено нічого не буде
+console.log(nums);
+
+//на місце мінімального вставити передане значення
+
+const nums2 = [2, 2, 5, 8, 5, 9, 6, 3, 2, 5];
+
+function getMin(array) {
+  let minElem = array[0]; //роблю припущення що перший елемент і є найменшим
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minElem) {
+      //порівнюю найменший з кожним елементом
+      minElem = array[i]; //якщо поточний буде менше мінімального, то переписую значення мінімального
+    }
   }
-  return resultSumma
+  return minElem;
 }
-//console.dir(sum)
-//console.log(sum(1,2,3,4,5));
 
-
-// функція приймає безліч приймає безліч аргументів і повертає масив, в якому будуть тільки парні значення з переданих аргументів, якщо таких не має - то пустий масив
-// getEvenArray(1,2,3,4) => [2,4]
-// getEvenArray(1,5) => []
-// getEvenArray(6,2,13,4,5,8) => [6,2,4,8]
-
+function changeMinElem(array, value) {
+  let minElem = getMin(array);
+  let startIndex = 0;
+  while (true) {
+    let startIndexMin = array.indexOf(minElem, startIndex++);
+    if (startIndexMin === -1) {
+      return;
+    }
+    array.splice(startIndexMin, 1, value);
+  }
+}
+console.log(nums2);
+changeMinElem(nums2, 888);
+console.log(nums2);
