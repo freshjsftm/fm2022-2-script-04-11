b2acce6514687736a587344fa2c8ea7271b9e364
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-22 10:13:43 +0200

    Refacctor push & constructor MyArray

--- MyArray.js
+++ MyArray.js
@@ -1,30 +1,36 @@
 function MyArrayPrototype() {
-  this.push = function (value) {
-    this[this.length++] = value;
+  //перепешіть пуш так, щоб він приймав безліч аргументів і insert їх в кінець масиву
+  this.push = function () {
+    for (let i = 0; i < arguments.length; i++) {
+      //перебираємо кожен аргумент
+      this[this.length++] = arguments[i]; //значення аргументо всталяємо в кінець нашого масиву
+    }
     return this.length;
   };
-  this.pop = function(){
-    //перевіряємо довжину, щоб вона не стала від'ємною
-    if(this.length === 0){
-      return; //return undefined; - так не пишуть!!!
+
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
     }
-    //отримую останній і зберігаю у змінну
-    const lastItem = this[this.length-1];
-    //видаляю останній
-    delete this[this.length-1];
-    //змінюю довжину
-    this.length--; //this.length = this.length-1;
-    //повертаю останній зі змінної
+    const lastItem = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
     return lastItem;
-  }
+  };
 }
 function MyArray() {
   this.length = 0;
+  if(arguments.length === 1){
+    for (let i = 0; i < arguments[0]; i++) {
+      this.push(undefined);
+    }
+    return;
+  }
+  for (let i = 0; i < arguments.length; i++) {
+    this.push(arguments[i]);
+  }
 }
 MyArray.prototype = new MyArrayPrototype();
-const myArr = new MyArray();
-myArr.push(12);
+const myArr = new MyArray(4, 5, 5, 8);
+myArr.push(12, 14, 12);
 myArr.push(56);
-
-
-
