42e9409d4bfed42144152022cf1308a79187fc37
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-28 11:06:38 +0200

    Add try...catch...finally

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function recursion(number) {
 }
 //recursion(3);
 
-
 /*
 2^4 = 2 * 2^3
 2^3 = 2 * 2^2
@@ -19,15 +18,50 @@ function recursion(number) {
 2^0 = 1       - basic
 */
 
-function power(num, exp){
+/**
+ *
+ * @param {number} num
+ * @param {number} exp
+ * @returns {number}
+ */
+function power(num, exp) {
   //debugger
-  if(exp===0){
-    return 1n;
+  if (
+    (typeof num !== "number" && typeof num !== "bigint") ||
+    typeof exp !== "number"
+  ) {
+    throw new TypeError("Type not right!");
   }
-  return num * power(num, exp-1);
+  if(exp<0){
+    throw new Error("Exp must be positive!");
+  }
+  if (exp === 0) {
+    return typeof num === "bigint" ? 1n : 1 ;
+  }
+  return num * power(num, exp - 1);
 }
+
+try {
+  console.log("try");
+  console.log(power(2n,4));
+  console.log(power(2,-4));
+  power("two", "four");
+} catch (error) {
+  console.log("catch");
+  console.log(error);
+} finally {
+  console.log("finally");
+}
+//power('two', 'four');
 
 //console.log(power(2n,42000));
 
+console.log("other script");
+console.log("other script");
+console.log("other script");
 
-
+/*
+try{}catch{}
+try{}finally{}
+try{}catch{}finally{}
+*/
