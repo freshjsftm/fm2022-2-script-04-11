7176e6cab1f0a6c819eb3e9d4bd3fc50224774fb
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-12-06 11:15:19 +0200

    Add example big O

--- js/index.js
+++ js/index.js
@@ -1,24 +1,54 @@
 "use strict";
 
-const symbol = Symbol("create new symbol"); //uniqueValue
-const symbol1 = Symbol("create new symbol"); //uniqueValue
-// console.log(symbol);
-// console.log(symbol1);
+const arr = [1, 2, 5, 6, 8, 9, 11, 15, 16, 20];
 
-const obj1 = {
-  login: "qwerty",
-  symbol: 777,
-  [symbol]: "value with key symbol",
+//константна  O(1)
+console.log(arr[3]);
+
+//лінійна O(n)  indexOf()
+const linearSearch = (arr, key) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === key) {
+      return i;
+    }
+  }
+  return -1;
 };
-// console.log(obj1);
-// console.log(obj1.symbol);
-// console.log(obj1[symbol]);
+console.log(linearSearch(arr, 7));
 
-// delete obj1[symbol];
+//квадратична O(n^2)
+const createTableMult = (limit) => {
+  const tableMult = [];
+  for (let i = 1; i <= limit; i++) {
+    //const subTable = [];
+    for (let j = 1; j <= limit; j++) {
+      tableMult.push(`${i}*${j}=${i * j}`);
+    }
+    //tableMult.push(subTable)
+  }
+  return tableMult;
+};
+console.log(createTableMult(200));
 
-const arr = [1,2,3,4,5];
-// console.log(...arr)
-// for (const iterator of arr) {
-//   console.log(iterator)
-// }
-
+//логаріфмічна O(log(n))
+const binarySearch = (arr, key)=>{
+  let start = 0;
+  let end = arr.length-1;
+  let middle;
+  while(start<=end){
+    middle = Math.round((start+end)/2); 
+    if(arr[middle]===key){
+      return middle;
+    }
+    if(arr[middle]<key){
+      start = middle + 1;
+    }else{ //arr[middle]>key
+      end = middle - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(arr, 2));
