1390dbc0f392347511c8e7622591f0933f44a084
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-12-08 11:05:26 +0200

    Add Set

--- js/index.js
+++ js/index.js
@@ -1,57 +1,36 @@
 "use strict";
 
-const dictionary = new Map();
-dictionary.set("йти", "go");
-dictionary.set("їсти", "eat");
-dictionary.set("спати", "sleep");
-dictionary.set("кіт", "cat");
-dictionary.set("пес", "dog");
-dictionary.set("білка", "squirrel");
-dictionary.set("розумний", "clever");
-dictionary.set("веселий", "happy");
-dictionary.set("гарний", "nice");
-dictionary.set("багатий", "rich");
-dictionary.set("ліс", "forest");
-dictionary.set("у", "in");
-dictionary.set("з", "with");
-dictionary.set("день", "day");
+const dictionary = new Map()
+dictionary.set("йти", 'go')
+dictionary.set("їсти", 'eat');
+dictionary.set("спати", 'sleep');
+dictionary.set("кіт", 'cat');
+dictionary.set("пес", 'dog');
+dictionary.set("білка", 'squirrel');
+dictionary.set("розумний", 'clever');
+dictionary.set("веселий", 'happy');
+dictionary.set("гарний", 'nice');
+dictionary.set("багатий", 'rich');
+dictionary.set("ліс", 'forest');
+dictionary.set("у", 'in');
+dictionary.set("з", 'with');
+dictionary.set("день", 'day');
 
-if (dictionary.has("qwe")) {
-  dictionary.delete("qwe");
-} else {
-  console.log('not found')
-}
+const set1 = new Set(dictionary.keys());
+const arr = [1,2,5,6,5,2,1,5,5,6,8,2,6];
+const arr2 = [2,9,6,5,4,8,5,3,58,9,5];
+// const set2 = new Set([...arr, ...arr2]);
+// console.log(set2)
+//const uniqueArr = [...set2]
+const uniqueArr = [...new Set([...arr, ...arr2])];
+console.log(uniqueArr);
+//const set1 = new Set("йти","їсти");
 
-const arrays = [...dictionary.entries()]   //dictionary.keys();
+set1.add(1);
+set1.add('1');
+set1.add([1]);
+set1.add({1:1});
+set1.add('1');
+set1.add('1');
 
-// const keys = dictionary.keys();
-// console.log(keys)
-for (const key of dictionary.keys()) {
-  console.log(key)
-}
-// for (const val of dictionary.values()) {
-//   console.log(val)
-// }
-// for (const [key, value] of dictionary.entries()) {
-//   console.log(key, value);
-// }
-
-const str =
-  "Розумний Пес з веселий кіт Йти вечір швидко у ліс їсти Гарний білка .";
-
-// const translate = (str, dict = dictionary)=>{
-//   const words = str.toLowerCase().split(' ');
-//   const translateWords = words.map((word)=>{
-//     return dict.has(word)?dict.get(word):word
-//   })
-//   return translateWords.join(' ')
-// }
-
-const translate = (str, dict = dictionary, separator = " ") =>
-  str
-    .toLowerCase()
-    .split(separator)
-    .map((word) => (dict.has(word) ? dict.get(word) : word))
-    .join(separator);
-
-//console.log(translate(str));
+console.log(set1)
